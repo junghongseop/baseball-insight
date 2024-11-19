2c29e40f2cd83e71b68ed52ce6c84be529f68d90
junghongseop <a01082372487@gmail.com>
2024-11-19 09:53:17 +0900

    init: 기본 세팅

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  root: true,
+  extends: ['@maru/eslint-config/react-ts-noimport', 'plugin:@next/next/recommended'],
 };
-
-export default nextConfig;
