7e1cce4d43ccfec873251409d646147019adbd9e
junghongseop <a01082372487@gmail.com>
2024-12-03 10:24:16 +0900

    fix: 메타테그 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Victory",
-  description: "KBO 데이터와 나의 팀 춫천",
+  title: 'Victory',
+  description: 'KBO 데이터와 나의 팀 추천',
 };
 
 export default function RootLayout({
@@ -12,9 +12,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
