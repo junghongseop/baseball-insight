d1014bd36409505db73f5c029435ed26838e1679
junghongseop <a01082372487@gmail.com>
2024-12-13 03:54:52 +0900

    fix: 메인 화면 구성 완성 및 글로벌 스타일드 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,43 @@
+'use client';
+
+import { Content } from '@/components/main';
+import { AppLayout } from '@/layouts';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css');
+
+body, html {
+  margin: 0;
+  padding: 0;
+  font-family: 'Pretendard', sans-serif;
+  font-weight: 300;
+}
+
+
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+`;
+
 export default function Home() {
   return (
-    <div>메인 페이지</div>
+    <>
+      <GlobalStyle />
+      <AppLayout header footer>
+        <StyledMainPage>
+          <Content />
+        </StyledMainPage>
+      </AppLayout>
+    </>
   );
 }
+
+const StyledMainPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+`;
