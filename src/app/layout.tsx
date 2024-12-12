0c7b430170b5f43055b94c744b976021fa48e7ac
junghongseop <a01082372487@gmail.com>
2024-12-12 11:11:12 +0900

    fix: 메타데이터 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Victory - 당신만의 KBO 골든 글러브 & 팀 추천",
-  description: "KBO 리그를 올해의 골든 글러브 수상자 예측과 응원할 팀을 추천합니다.",
+  title: 'Victory - 당신만의 KBO 골든 글러브 & 팀 추천',
+  description: 'KBO 리그를 올해의 골든 글러브 수상자 예측과 응원할 팀을 추천합니다.',
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
