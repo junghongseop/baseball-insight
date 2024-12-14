1ef035fe7c2f75885b62c7bad24bc1e6d95d1844
junghongseop <a01082372487@gmail.com>
2024-12-15 01:23:33 +0900

    add: 외부 이미지 경로 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lgcxydabfbch3774324.cdn.ntruss.com',
+        pathname: '/**', // 모든 경로를 허용
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+        pathname: '/**', // 이전의 placeholder 호스트도 허용
+      },
+    ],
+  },
+};
 
 export default nextConfig;
