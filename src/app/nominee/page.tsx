82be21a446b63133688a1aeb67739f4acc2b45ca
junghongseop <a01082372487@gmail.com>
2024-12-15 02:25:47 +0900

    fix: 페이지에서 후보자 리스트 보여주도록 함

--- src/app/nominee/page.tsx
+++ src/app/nominee/page.tsx
@@ -1,5 +1,25 @@
+'use client';
+
+import { Content } from '@/components/nominee';
+import { AppLayout } from '@/layouts';
+import styled from 'styled-components';
+
 const NomineePage = () => {
-  return <div>골든 글러브 후보자 페이지</div>;
+  return (
+    <AppLayout header footer>
+      <StyledNomineePage>
+        <Content />
+      </StyledNomineePage>
+    </AppLayout>
+  );
 };
 
 export default NomineePage;
+
+const StyledNomineePage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0;
+  width: 100%;
+`;
