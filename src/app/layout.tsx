21256ee2acb6223106aa1d16ec44aa6d91ae2977
junghongseop <a01082372487@gmail.com>
2024-12-15 22:13:54 +0900

    fix: 메타데이터 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Victory - 당신만의 KBO 골든 글러브 & 팀 추천',
-  description: 'KBO 리그를 올해의 골든 글러브 수상자 예측과 응원할 팀을 추천합니다.',
+  title: 'Victory - 당신만의 KBO 골든 글러브 수상자 예측',
+  description: 'KBO 리그를 올해의 골든 글러브 수상자 예측합니다.',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
